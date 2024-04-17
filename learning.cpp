80230dfeb6596006ebb1f53bc35e52d57248b69b
MohamESalem <159151573+MohamESalem@users.noreply.github.com>
2024-04-17 20:59:40 +0200

    Better hash table implementation using separate chaining

--- learning.cpp
+++ learning.cpp
@@ -1,214 +1,109 @@
-
 #include <iostream>
-#include <vector>
-#include <stdexcept>
-
+#include <list>
+#include <cstring>
 using namespace std;
 
-//exception
-class BadIterator : public std::exception {
-public:
-    const char* what() const throw() {
-        return "Invalid iterator";
-    }
+//hash table using separate chaining
+
+class hashTable {
+    private:
+        static const int hashGroups = 10;
+        list<pair<int, string>> table[hashGroups];
+    
+    public:
+        bool isEmpty() const;
+        int hashFunction(int key);
+        void insertItem(int key, string value);
+        void removeItem(int key);
+        string searchItem(int key);
+        void printTable();
 };
 
-// this incomplete declaration is for the friend class
-template <typename Object>
-class linkedList;
-
-template <typename Object>
-class linkedListItr;
-
-// Node class
-template <typename Object>
-class linkedListNode
+bool hashTable::isEmpty() const
 {
-    linkedListNode(const Object &theElement = Object(), linkedListNode *n = NULL) : element(theElement), next(n) {}
-    Object element;
-    linkedListNode *next;
-    friend class linkedList<Object>;
-    friend class linkedListItr<Object>;
-};
-
-// Iterator classs
-template <typename Object>
-class linkedListItr
-{
-public:
-    linkedListItr() : current(NULL) {}
-    bool isValid() const
+    int sum;
+    for (int i = 0; i < hashGroups; i++)
     {
-        return current != NULL;
+        sum += table[i].size();
     }
+    
+    return !sum;
+}
 
-    void advance()
+int hashTable::hashFunction(int key)
+{
+    return key& hashGroups;
+}
+
+void hashTable::insertItem(int key, string value)
+{
+    int hashValue = hashFunction(key);
+    auto& cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++)
     {
-        if (isValid())
-            current = current->next;
-    }
-
-    const Object &retrieve() const
-    {
-        if (!isValid())
-            throw BadIterator();
-        return current->element;
-    }
-
-private:
-    linkedListNode<Object> *current;
-    linkedListItr(linkedListNode<Object> *theNode) : current(theNode) {}
-
-    friend class linkedList<Object>;
-};
-
-// linked list class
-template <typename Object>
-class linkedList
-{
-public:
-    linkedList();
-    linkedList(const linkedList &rhs);
-    ~linkedList();
-
-    bool isEmpty() const;
-    void makeEmpty();
-    linkedListItr<Object> zeroth() const;
-    linkedListItr<Object> first() const;
-    void insert(const Object &x, const linkedListItr<Object> &p);
-    linkedListItr<Object> find(const Object &x) const;
-    linkedListItr<Object> findPrevious(const Object &x) const;
-    void remove(const Object &x);
-    const linkedList &operator=(const linkedList &rhs);
-
-private:
-    linkedListNode<Object> *header;
-};
-
-template <typename Object>
-linkedList<Object>::linkedList()
-{
-    header = new linkedListNode<Object>;
-}
-
-template <typename Object>
-bool linkedList<Object>::isEmpty() const
-{
-    return header->next == NULL;
-}
-
-template <typename Object>
-linkedListItr<Object> linkedList<Object>::zeroth() const
-{
-    return linkedListItr<Object>(header);
-}
-
-template <typename Object>
-linkedListItr<Object> linkedList<Object>::first() const
-{
-    return linkedListItr<Object>(header->next);
-}
-
-template <typename Object>
-void printList(const linkedList<Object> &theList)
-{
-    if (theList.isEmpty())
-        cout << "Empty List:" << endl;
-    else
-    {
-        linkedListItr<Object> itr = theList.first();
-        for (; itr.isValid(); itr.advance())
+        if (bItr->first == key)
         {
-            cout << itr.retrieve() << " ";
+            keyExists = true;
+            bItr->second = value;
+            break;
         }
+        
     }
-    cout << endl;
-}
 
-template <typename Object>
-linkedListItr<Object> linkedList<Object>::find(const Object &x) const
-{
-
-    linkedListNode<Object> *p = header->next;
-
-    while (p != NULL && p->element != x)
-        p = p->next;
-
-    return linkedListItr<Object>(p);
-}
-
-template <typename Object>
-void linkedList<Object>::remove(const Object &x)
-{
-
-    linkedListNode<Object> *p = findPrevious(x).current;
-
-    if (p->next != NULL)
+    if (!keyExists)
     {
-
-        linkedListNode<Object> *oldNode = p->next;
-        p->next = p->next->next; // Bypass
-        delete oldNode;
+        cell.emplace_back(key, value);
     }
+
+    return;
+    
+    
 }
 
-template <typename Object>
-linkedListItr<Object> linkedList<Object>::findPrevious(const Object &x) const
+void hashTable::removeItem(int key)
 {
-
-    linkedListNode<Object> *p = header;
-
-    while (p->next != NULL && p->next->element != x)
-        p = p->next;
-
-    return linkedListItr<Object>(p);
-}
-
-template <typename Object>
-
-void linkedList<Object>::insert(const Object &x, const linkedListItr<Object> &p)
-{
-
-    if (p.current != NULL)
-        p.current->next = new linkedListNode<Object>(x, p.current->next);
-}
-
-// Make the list logically empty.
-template <typename Object>
-void linkedList<Object>::makeEmpty()
-{
-    while (!isEmpty())
-        remove(first().retrieve());
-}
-
-// Destructor.
-template <typename Object>
-linkedList<Object>::~linkedList()
-{
-    makeEmpty();
-    delete header;
-}
-
-template <typename Object>
-linkedList<Object>::linkedList(const linkedList<Object> &rhs)
-{
-    header = new linkedListNode<Object>;
-    *this = rhs;
-}
-
-// Deep copy of linked lists.
-template <typename Object>
-const linkedList<Object> &
-linkedList<Object>::operator=(const linkedList<Object> &rhs)
-{
-
-    if (this != &rhs)
+    int hashValue = hashFunction(key);
+    auto& cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++)
     {
-        makeEmpty();
-        linkedListItr<Object> ritr = rhs.first();
-        linkedListItr<Object> itr = zeroth();
-        for (; ritr.isValid(); ritr.advance(), itr.advance())
-            insert(ritr.retrieve(), itr);
+        if (bItr->first == key)
+        {
+            keyExists = true;
+            bItr = cell.erase(bItr);
+            break;
+        }
+        
     }
-    return *this;
+
+    if (!keyExists)
+    {
+        cout<< "Item doesn't exist." <<endl;
+    }
+
+    return;
 }
+
+void hashTable::printTable()
+{
+    for (int i = 0; i < hashGroups; i++)
+    {
+        if (table[i].size()==0) continue;
+
+        auto bItr = table[i].begin();
+        for (; bItr!=table[i].end(); bItr++)
+        {
+            cout<< "Key: " << bItr->first << " Value: " << bItr->second <<endl;
+        }
+        
+        
+    }
+    return;
+    
+}
+
+//better hash table implementation
+// reference: https://www.youtube.com/watch?v=2_3fR-k-LzI
