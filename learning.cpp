610dbbbaf638e60dc2cc606131a91900b998d8bb
MohamESalem <159151573+MohamESalem@users.noreply.github.com>
2024-04-18 09:34:37 +0200

    Simple implementation of add function of a binary search tree

--- learning.cpp
+++ learning.cpp
@@ -5,75 +5,60 @@
 #include <vector>
 using namespace std;
 
-class Graph 
-{
-    int V;
-    // pointer to an array containing adjacency lists
-    vector<vector<int>> adj;
-    void _DFS(int v, vector<bool>& visited);
+class BST {
+private:
+    struct Node{
+        int data;
+        Node* left;
+        Node* right;
+    };
+
+    Node* root = nullptr;
+
+
 
 public:
-    Graph(int V);
-    void addEdge(int v, int w);
-    void BFS(int s);
-    void DFS(int v);
-};
+    void add(int value){
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->left = nullptr;
+        newNode->right= nullptr;
 
-//getting the number of vertices
-Graph::Graph(int V)
-{
-    this->V = V;
-    adj.resize(V);
-}
-
-//takes two vertices and connects them
-void Graph::addEdge(int v, int w)
-{
-    adj[v].push_back(w);
-}
-
-//
-void Graph::_DFS(int v, vector<bool>& visited)
-{
-    visited[v] = true;
-    cout << v << " ";
-
-    for (auto i = adj[v].begin(); i!= adj[v].end(); i++)
-    {
-        if (!visited[*i])
+        if (root=nullptr)
         {
-            _DFS(*i, visited);
+            root = newNode;
+            return;
         }
-    }
-}
 
-void Graph::BFS(int s)
-{
-    //mark all the vertices as not visited
-    vector<bool> visited(V, false);
-    queue<int> q;
-    visited[s] = true;
-    q.push(s);
-
-    while (!q.empty())
-    {
-        s = q.front();
-        cout << s << " ";
-        q.pop();
-        for (auto i = adj[s].begin(); i!= adj[s].end(); i++)
+        Node* temp = root;
+        Node* parent = nullptr;
+        while (temp!=nullptr)
         {
-            if (!visited[*i])
+            parent = temp;
+            if (value<=temp->data)
             {
-                visited[*i] = true;
-                q.push(*i);
+                temp = temp->left;
             }
-        }
-    }
-}
 
-void Graph::DFS(int v)
-{
-    //mark all the vertices as not visited
-    vector<bool> visited(V, false);
-    _DFS(v, visited);
-}
+            else
+            {
+                temp=temp->right;
+            }
+            
+            
+        }
+
+        if (value<=parent->data)
+        {
+            parent->left = newNode;
+        }
+
+        else
+        {
+            parent->right = newNode;
+        }
+        
+    }
+
+
+};
